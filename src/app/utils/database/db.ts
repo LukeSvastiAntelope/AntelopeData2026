410fea708aadf81cd3f33af1c9e9bdf61b399288
ledgerninja0x <sweetheart199558@gmail.com>
2024-12-22 01:33:48 +0200

    fix: sql connect issue

--- src/app/utils/database/db.ts
+++ src/app/utils/database/db.ts
@@ -5,10 +5,17 @@ const connectionParams = {
     port: parseInt(process.env.MYSQL_PORT || '3306'),
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 }
 
 export const openSql = async () => {
-    const connection = await mysql.createConnection(connectionParams);
+    const connection = await mysql.createPool(connectionParams);
     return connection;
 }
