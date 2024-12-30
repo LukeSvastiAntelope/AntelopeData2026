d5d9bef85b4f7afaecb87884f31b0a04a26b5b3d
LedgerNinja0x <sweetheart199558@gmail.com>
2024-12-30 09:36:35 -0700

    fix: update about page

--- src/app/(secure)/about/page.tsx
+++ src/app/(secure)/about/page.tsx
@@ -9,9 +9,9 @@ import { IAgentProfile } from "@/app/utils/interface";
 
 export default function AboutPage() {
   const [agent, setAgent] = useState<IAgentProfile | null>(null);
+  const fetchData = useFetch();
   
   useEffect(() => {
-    const fetchData = useFetch();
     const fetchAgentProfile = async () => {
       try {
         const response = await fetchData.get("/api/getAgentProfile");
