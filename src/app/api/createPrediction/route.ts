6762468772052976ecf716663fdb7b38e0c530b4
ledgerninja0x <sweetheart199558@gmail.com>
2025-01-14 23:01:26 +0100

    fix: update all field check

--- src/app/api/createPrediction/route.ts
+++ src/app/api/createPrediction/route.ts
@@ -70,10 +70,9 @@ export async function POST(req: NextRequest) {
             data.str_thumb = result.images_results[0].original || "";
         }
 
-        if (data.source === "custom" && (!data.description || !data.source || !data.resolution_date || !data.bet_amount || !data.creator_choice)) {
+        if (prediction.type === "custom" && (!data.description || !data.source || !data.resolution_date || !data.bet_amount || !data.creator_choice)) {
             return Response.json({ status: false, message: 'All fields are required' });
-
-        } else if (data.source !== "custom" && (!data.event_id || !data.team_a || !data.team_b || !data.resolution_date || !data.bet_amount || !data.creator_choice || !data.league_id || !data.description)) {
+        } else if (!data.event_id || !data.team_a || !data.team_b || !data.resolution_date || !data.bet_amount || !data.creator_choice || !data.league_id || !data.description) {
             return Response.json({ status: false, message: 'All fields are required' });
         }
         const user = await UserRepo.getUserById(data.creator_id.toString());
