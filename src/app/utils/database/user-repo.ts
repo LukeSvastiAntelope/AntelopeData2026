7324ab4a1901f1186ccb0d09c525bc07d065fb06
LedgerNinja0x <trinklestar07@gmail.com>
2025-01-22 12:43:53 -0700

    feat: update status

--- src/app/utils/database/user-repo.ts
+++ src/app/utils/database/user-repo.ts
@@ -570,7 +570,15 @@ async function getBetById(id: string) {
     try {
         const db = await getMySQLConnection();
         const [rows] = await db.execute<(IBet & RowDataPacket)[]>(
-            `SELECT bets.*, predictions.creator_choice, predictions.str_thumb, predictions.outcome, predictions.resolution_date, predictions.predicted_outcome, predictions.source, predictions.description
+            `SELECT bets.*, 
+            predictions.creator_choice, 
+            predictions.str_thumb, 
+            predictions.outcome, 
+            predictions.resolution_date, 
+            predictions.predicted_outcome, 
+            predictions.source, 
+            predictions.description,
+            predictions.status
             FROM bets 
             JOIN predictions ON bets.prediction_id = predictions.id 
             WHERE bets.id = ?`, 
