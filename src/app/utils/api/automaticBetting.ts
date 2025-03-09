206f7412cc907ef1a2a2af2d8ec5eff59e21fc54
LedgerNinja0x <trinklestar07@gmail.com>
2025-03-09 09:34:18 -0700

    feat: update betting conifg

--- src/app/utils/api/automaticBetting.ts
+++ src/app/utils/api/automaticBetting.ts
@@ -113,7 +113,7 @@ export class AutomaticBettingAgent {
                 if (i + CHUNK_SIZE < predictions.length) {
                     await new Promise(resolve => setTimeout(resolve, 500));
                 }
-                if (i > 20) {
+                if (i > 15) {
                     console.log(`Processed ${i + CHUNK_SIZE} predictions`);
                     break;
                 }
